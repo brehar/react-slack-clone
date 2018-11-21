a984407043ff67a989ac6fccfd83e1f5a712d719
Brett Hartman <brett.hartman@mac.com>
2018-11-21 05:17:39 -0800

    Implement basic routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import App from './App';
+import App from './components/App';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/" component={App} />
+			<Route path="/login" component={Login} />
+			<Route path="/register" component={Register} />
+		</Switch>
+	</Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 registerServiceWorker();
