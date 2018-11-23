05de7882359c60de3c1e1a3a583d33c665fa5ddb
Brett Hartman <brett.hartman@mac.com>
2018-11-22 17:07:07 -0800

    Scaffold out the ColorPanel, MessagesHeader, and MessageForm components

--- src/components/colorPanel/ColorPanel.js
+++ src/components/colorPanel/ColorPanel.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
+import { Button, Divider, Menu, Sidebar } from 'semantic-ui-react';
+
 class ColorPanel extends Component {
 	render() {
-		return <div>ColorPanel</div>;
+		return (
+			<Sidebar as={Menu} icon="labeled" inverted vertical visible width="very thin">
+				<Divider />
+				<Button icon="add" size="small" color="blue" />
+			</Sidebar>
+		);
 	}
 }
 
--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
+import { Comment, Segment } from 'semantic-ui-react';
+
+import firebase from '../../firebase';
+
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
+
 class Messages extends Component {
+	state = {
+		//
+	};
+
 	render() {
-		return <div>Messages</div>;
+		return (
+			<React.Fragment>
+				<MessagesHeader />
+				<Segment>
+					<Comment.Group className="messages">{/* TODO: Display Messages */}</Comment.Group>
+				</Segment>
+				<MessageForm />
+			</React.Fragment>
+		);
 	}
 }
 
